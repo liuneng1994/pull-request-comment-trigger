9b7dfa7669bcaeea1da22f7d43646db7c33040de
liuneng <1398775315@qq.com>
2022-11-09 13:52:15 +0800

    change node js version

--- index.js
+++ index.js
@@ -5,61 +5,19 @@ const { context, GitHub } = require("@actions/github");
 
 async function run() {
     const trigger = core.getInput("trigger", { required: true });
+    const regexp = new RegExp(trigger, "g")
+    const triggered= "triggered"
+    const trigger_var = "trigger_var"
 
-    const reaction = core.getInput("reaction");
-    const { GITHUB_TOKEN } = process.env;
-    if (reaction && !GITHUB_TOKEN) {
-        core.setFailed('If "reaction" is supplied, GITHUB_TOKEN is required');
-        return;
-    }
-
-    const body =
-        (context.eventName === "issue_comment"
-        // For comments on pull requests
-            ? context.payload.comment.body
-            // For the initial pull request description
-            : context.payload.pull_request.body) || '';
-    core.setOutput('comment_body', body);
-
-    if (
-        context.eventName === "issue_comment" &&
-        !context.payload.issue.pull_request
-    ) {
-        // not a pull-request comment, aborting
-        core.setOutput("triggered", "false");
-        return;
-    }
-
-    const { owner, repo } = context.repo;
-
-
-    const prefixOnly = core.getInput("prefix_only") === 'true';
-    if ((prefixOnly && !body.startsWith(trigger)) || !body.includes(trigger)) {
-        core.setOutput("triggered", "false");
-        return;
-    }
-
-    core.setOutput("triggered", "true");
-
-    if (!reaction) {
-        return;
-    }
-
-    const client = new GitHub(GITHUB_TOKEN);
-    if (context.eventName === "issue_comment") {
-        await client.reactions.createForIssueComment({
-            owner,
-            repo,
-            comment_id: context.payload.comment.id,
-            content: reaction
-        });
+    const body = context.payload.pull_request.body || '';
+    console.log("pull request body: \n"+body)
+    if (regexp.test(body)) {
+        core.setOutput(triggered, "true")
+        console.log(regexp.exec(body))
+        const word = regexp.exec(body)[1]
+        core.setOutput(trigger_var, word)
     } else {
-        await client.reactions.createForIssue({
-            owner,
-            repo,
-            issue_number: context.payload.pull_request.number,
-            content: reaction
-        });
+        core.setOutput(triggered, "false")
     }
 }
 
